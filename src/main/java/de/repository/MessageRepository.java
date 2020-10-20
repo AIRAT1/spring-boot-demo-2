8c0b20265d4e2d5a0047940c23f0a48c58c2f556
Ayrat <ayrat1@mail.ru>
2020-10-20 19:41:18 +0300

    add paging

--- src/main/java/de/repository/MessageRepository.java
+++ src/main/java/de/repository/MessageRepository.java
@@ -1,8 +1,11 @@
 package de.repository;
 
 import de.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<Message, Integer> {
-    Iterable<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+    Page<Message> findAll(Pageable pageable);
 }
